4a957c78ed47470e59c2d1efc4a09b6f17bf45f5
SukhovetskaTetiana <garmonia.t@gmail.com>
2024-10-10 10:28:45 +0200

    added date

---
+++ App/43_date.js
@@ -0,0 +1,17 @@
+const date = new Date(); //2023-01-22T09:44:48.175Z
+date.getDate(); // month`s date: 22
+date.getMonth(); // Month with 0 index: 0
+date.getFullYear(); // Year: 2023
+date.getHours(); // Hours: 9
+date.getMinutes(); // Minutes: 44
+date.getSeconds(); // Seconds: 48
+date.getMilliseconds(); // Millisecond: 175
+date.getTime(); // Time: 1648101488175
+date.setDate(23); // Set date: 23
+date.setMonth(3); // Set month: 3
+date.setFullYear(2024); // Set year: 2024
+date.setHours(10); // Set hours: 10
+date.setMinutes(45); // Set minutes: 45
+date.setSeconds(49); // Set seconds: 49
+date.setMilliseconds(176); // Set Milliseconds: 176
+date.setTime(1648101488176); // Set time: 1648101488176
