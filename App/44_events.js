cca6e92dfb93268f1a51b08737a3d0dfe4992265
SukhovetskaTetiana <garmonia.t@gmail.com>
2024-10-17 20:12:51 +0200

    added 44_events

---
+++ App/44_events.js
@@ -0,0 +1,41 @@
+// when use cklicks ->
+<input type='text' onclick='' />
+  
+// when user double cklicks ->
+<input type='text' ondblclick='' />
+
+// when user moves the mouse over an element, it`s called mouse down ->
+<input type='text' onmousedown='' />
+
+// when an element looses focus ->
+<input type='text' onblur='' />
+  
+// when an element gets focus ->
+<input type='text' onfocus='' />
+
+// when a user moves the mouse over an element ->
+<input type='text' onmouseover='' />
+  
+// when a user moves the mouse out of an element ->
+<input type='text' onmouseout='' />
+
+// when there is a change ->
+<input type='text' onchange />
+
+// when a user presses a key
+<input type='text' onkeydown='' />
+  
+// when a user releases a key
+<input type='text' onkeyup='' />
+  
+// when a user presses a key
+<input type='text' onkeypress='' />
+  
+// when a user submits a form
+<form onsubmit=''></form>
+  
+// when a user resets a form
+<form onreset=''></form>
+
+// when a user selects a text
+<input type='text' onselect='' />
